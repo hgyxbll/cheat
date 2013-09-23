3e23fe702e2cc7123694469b547b27d6c2cd7700
Sampsa Kiiskinen <tuplanolla@gmail.com>
2013-09-23 18:42:31 +0300

    Zapped chunks of spaces to tabs with the exception of one line, where they have semantic value.

--- cheat.h
+++ cheat.h
@@ -13,28 +13,28 @@
 #include <stdarg.h>
 
 enum cheat_test_status {
-    CHEAT_SUCCESS,
-    CHEAT_FAILURE,
-    CHEAT_IGNORE,
-    CHEAT_SEGFAULT
+	CHEAT_SUCCESS,
+	CHEAT_FAILURE,
+	CHEAT_IGNORE,
+	CHEAT_SEGFAULT
 };
 
 struct cheat_test_suite {
-    size_t test_count;
-    size_t test_failures;
-    enum cheat_test_status last_test_status;
-    char **log;
-    char *argv0;
-    size_t log_size;
-    bool nofork;
-    FILE *captured_stdout;
+	size_t test_count;
+	size_t test_failures;
+	enum cheat_test_status last_test_status;
+	char **log;
+	char *argv0;
+	size_t log_size;
+	bool nofork;
+	FILE *captured_stdout;
 };
 
 typedef void cheat_test(struct cheat_test_suite *suite);
 
 struct cheat_test_s {
-    char const *name;
-    cheat_test *test;
+	char const *name;
+	cheat_test *test;
 };
 
 /* Public interface: helpers */
@@ -57,118 +57,118 @@ struct cheat_test_s {
 
 static void cheat_suite_init(struct cheat_test_suite *suite, char *argv0)
 {
-    memset(suite, 0, sizeof (struct cheat_test_suite));
-    suite->captured_stdout = stdout;
-    suite->argv0 = argv0;
+	memset(suite, 0, sizeof (struct cheat_test_suite));
+	suite->captured_stdout = stdout;
+	suite->argv0 = argv0;
 }
 
 static void cheat_suite_summary(struct cheat_test_suite *suite)
 {
-    if (suite->log) {
-        size_t i;
+	if (suite->log) {
+		size_t i;
 
-        fputs("\n", suite->captured_stdout);
-        for (i = 0; i < suite->log_size; ++i) {
-            fputs(suite->log[i], suite->captured_stdout);
-            free(suite->log[i]);
-        }
+		fputs("\n", suite->captured_stdout);
+		for (i = 0; i < suite->log_size; ++i) {
+			fputs(suite->log[i], suite->captured_stdout);
+			free(suite->log[i]);
+		}
 
-        free(suite->log);
-    }
+		free(suite->log);
+	}
 
-    fprintf(suite->captured_stdout, "\n%zu failed tests of %zu tests run.\n", suite->test_failures, suite->test_count);
+	fprintf(suite->captured_stdout, "\n%zu failed tests of %zu tests run.\n", suite->test_failures, suite->test_count);
 }
 
 static void cheat_test_end(struct cheat_test_suite *suite)
 {
-    suite->test_count++;
+	suite->test_count++;
 
-    switch (suite->last_test_status) {
-        case CHEAT_SUCCESS:
-            fputc('.', suite->captured_stdout);
-            break;
-        case CHEAT_FAILURE:
-            fputc('F', suite->captured_stdout);
-            suite->test_failures++;
-            break;
-        case CHEAT_IGNORE:
-            fputc('I', suite->captured_stdout);
-            break;
-        case CHEAT_SEGFAULT:
-            fputc('S', suite->captured_stdout);
-            suite->test_failures++;
-            break;
-        default:
-            exit(-1);
-    }
+	switch (suite->last_test_status) {
+		case CHEAT_SUCCESS:
+			fputc('.', suite->captured_stdout);
+			break;
+		case CHEAT_FAILURE:
+			fputc('F', suite->captured_stdout);
+			suite->test_failures++;
+			break;
+		case CHEAT_IGNORE:
+			fputc('I', suite->captured_stdout);
+			break;
+		case CHEAT_SEGFAULT:
+			fputc('S', suite->captured_stdout);
+			suite->test_failures++;
+			break;
+		default:
+			exit(-1);
+	}
 }
 
 static void cheat_log_append(struct cheat_test_suite *suite, char *message, size_t len)
 {
-    char *buf;
+	char *buf;
 
-    if (len == 0) {
-        return;
-    }
+	if (len == 0) {
+		return;
+	}
 
-    buf = malloc(len + 1);
-    memcpy(buf, message, len);
+	buf = malloc(len + 1);
+	memcpy(buf, message, len);
 
-    buf[len] = '\0';
+	buf[len] = '\0';
 
-    suite->log_size++;
-    suite->log = realloc(suite->log, (suite->log_size + 1) * sizeof (char *));
-    suite->log[suite->log_size - 1] = buf; /* We give up our buffer! */
+	suite->log_size++;
+	suite->log = realloc(suite->log, (suite->log_size + 1) * sizeof (char *));
+	suite->log[suite->log_size - 1] = buf; /* We give up our buffer! */
 }
 
 static void cheat_test_assert(
-        struct cheat_test_suite *suite,
-        int result,
-        char const *assertion,
-        char const *filename,
-        int line)
+		struct cheat_test_suite *suite,
+		int result,
+		char const *assertion,
+		char const *filename,
+		int line)
 {
-    if (result != 0)
-        return;
+	if (result != 0)
+		return;
 
-    suite->last_test_status = CHEAT_FAILURE;
-    if (suite->nofork) {
-        char *buffer = NULL;
-        size_t len = 255;
-        size_t bufsize;
+	suite->last_test_status = CHEAT_FAILURE;
+	if (suite->nofork) {
+		char *buffer = NULL;
+		size_t len = 255;
+		size_t bufsize;
 
-        do {
-            bufsize = (len + 1);
-            buffer = realloc(buffer, bufsize);
-            const int what = snprintf(buffer, bufsize,
-                    "%s:%d: Assertion failed: '%s'.\n",
-                    filename,
-                    line,
-                    assertion);
-            if (what == -1)
-                exit(42);
-            len = (size_t )what;
-        } while (bufsize != (len + 1));
+		do {
+			bufsize = (len + 1);
+			buffer = realloc(buffer, bufsize);
+			const int what = snprintf(buffer, bufsize,
+					"%s:%d: Assertion failed: '%s'.\n",
+					filename,
+					line,
+					assertion);
+			if (what == -1)
+				exit(42);
+			len = (size_t )what;
+		} while (bufsize != (len + 1));
 
-        cheat_log_append(suite, buffer, bufsize);
-    } else {
-        fprintf(suite->captured_stdout,
-                "%s:%d: Assertion failed: '%s'.\n",
-                filename,
-                line,
-                assertion);
-    }
+		cheat_log_append(suite, buffer, bufsize);
+	} else {
+		fprintf(suite->captured_stdout,
+				"%s:%d: Assertion failed: '%s'.\n",
+				filename,
+				line,
+				assertion);
+	}
 }
 
 static int run_test(struct cheat_test_s const *test, struct cheat_test_suite *suite)
 {
-    suite->last_test_status = CHEAT_SUCCESS;
+	suite->last_test_status = CHEAT_SUCCESS;
 
-    cheat_set_up();
-    (test->test)(suite);
-    cheat_tear_down();
+	cheat_set_up();
+	(test->test)(suite);
+	cheat_tear_down();
 
-    return suite->last_test_status;
+	return suite->last_test_status;
 }
 
 #if _POSIX_C_SOURCE >= 200112L
@@ -184,104 +184,104 @@ static int run_test(struct cheat_test_s const *test, struct cheat_test_suite *su
 #endif
 
 static void run_isolated_test(
-        struct cheat_test_s const *test,
-        struct cheat_test_suite *suite)
+		struct cheat_test_s const *test,
+		struct cheat_test_suite *suite)
 {
 #if _POSIX_C_SOURCE >= 200112L
-    pid_t pid;
-    int pipefd[2];
+	pid_t pid;
+	int pipefd[2];
 
-    pipe(pipefd);
-    pid = fork();
+	pipe(pipefd);
+	pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        execl(suite->argv0, suite->argv0, test->name, NULL);
-        exit(EXIT_FAILURE);
-    } else {
-        int status;
-        char buf[255];
-        ssize_t len;
+	if (pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0) {
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		execl(suite->argv0, suite->argv0, test->name, NULL);
+		exit(EXIT_FAILURE);
+	} else {
+		int status;
+		char buf[255];
+		ssize_t len;
 
-        close(pipefd[1]);
-        while ((len = read(pipefd[0], buf, 255)) != 0) {
-            if (len == -1)
-                exit(42);
-            buf[len] = 0;
-            cheat_log_append(suite, buf, (size_t )len + 1);
-        }
+		close(pipefd[1]);
+		while ((len = read(pipefd[0], buf, 255)) != 0) {
+			if (len == -1)
+				exit(42);
+			buf[len] = 0;
+			cheat_log_append(suite, buf, (size_t )len + 1);
+		}
 
-        waitpid(pid, &status, 0);
-        close(pipefd[0]);
-        suite->last_test_status = WIFEXITED(status) ? WEXITSTATUS(status)
-                                                    : CHEAT_SEGFAULT;
-    }
+		waitpid(pid, &status, 0);
+		close(pipefd[0]);
+		suite->last_test_status = WIFEXITED(status) ? WEXITSTATUS(status)
+		                                            : CHEAT_SEGFAULT;
+	}
 
 #elif defined _WIN32
 
-    SECURITY_ATTRIBUTES sa;
-    sa.nLength = sizeof (SECURITY_ATTRIBUTES);
-    sa.bInheritHandle = TRUE;
-    sa.lpSecurityDescriptor = NULL;
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof (SECURITY_ATTRIBUTES);
+	sa.bInheritHandle = TRUE;
+	sa.lpSecurityDescriptor = NULL;
 
-    HANDLE stdoutPipe_read;
-    HANDLE stdoutPipe_write;
-    CreatePipe(&stdoutPipe_read, &stdoutPipe_write, &sa, 0);
+	HANDLE stdoutPipe_read;
+	HANDLE stdoutPipe_write;
+	CreatePipe(&stdoutPipe_read, &stdoutPipe_write, &sa, 0);
 
-    STARTUPINFO si = {
-        .cb = sizeof (STARTUPINFO),
-        .dwFlags = STARTF_USESTDHANDLES,
-        .hStdOutput = stdoutPipe_write
-    };
+	STARTUPINFO si = {
+		.cb = sizeof (STARTUPINFO),
+		.dwFlags = STARTF_USESTDHANDLES,
+		.hStdOutput = stdoutPipe_write
+	};
 
-    PROCESS_INFORMATION pi = {0};
+	PROCESS_INFORMATION pi = {0};
 
-    CHAR command[255];
-    snprintf(command, 255, "%s %s", suite->argv0, test->name);
+	CHAR command[255];
+	snprintf(command, 255, "%s %s", suite->argv0, test->name);
 
-    CreateProcess(
-        NULL,
-        command,
-        NULL,
-        NULL,
-        TRUE,
-        0,
-        NULL,
-        NULL,
-        &si,
-        &pi);
+	CreateProcess(
+		NULL,
+		command,
+		NULL,
+		NULL,
+		TRUE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi);
 
-    CloseHandle(stdoutPipe_write);
+	CloseHandle(stdoutPipe_write);
 
-    DWORD len;
-    DWORD maxlen = 255;
-    CHAR buffer[255];
+	DWORD len;
+	DWORD maxlen = 255;
+	CHAR buffer[255];
 
-    do {
-        ReadFile(stdoutPipe_read, buffer, maxlen, &len, NULL);
-        buffer[len] = '\0';
-        cheat_log_append(suite, buffer, len);
-    } while (len > 0);
+	do {
+		ReadFile(stdoutPipe_read, buffer, maxlen, &len, NULL);
+		buffer[len] = '\0';
+		cheat_log_append(suite, buffer, len);
+	} while (len > 0);
 
-    WaitForSingleObject(pi.hProcess, INFINITE);
+	WaitForSingleObject(pi.hProcess, INFINITE);
 
-    DWORD status;
-    GetExitCodeProcess(pi.hProcess, &status);
+	DWORD status;
+	GetExitCodeProcess(pi.hProcess, &status);
 
-    suite->last_test_status = (status & 0x80000000) ? CHEAT_SEGFAULT
-                                                    : status;
+	suite->last_test_status = (status & 0x80000000) ? CHEAT_SEGFAULT
+													: status;
 
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
 
 #else
 #warning "Running isolated tests not supported in this environment. You will need to use --nofork."
-    fputs("Running isolated tests not supported in this environment. Please use --nofork.\n", stderr);
-    exit(EXIT_FAILURE);
+	fputs("Running isolated tests not supported in this environment. Please use --nofork.\n", stderr);
+	exit(EXIT_FAILURE);
 #endif
 }
 
@@ -305,46 +305,46 @@ static const size_t cheat_test_count = sizeof (cheat_tests) / sizeof (struct che
 
 int main(int argc, char *argv[])
 {
-    struct cheat_test_suite suite;
-    size_t i;
+	struct cheat_test_suite suite;
+	size_t i;
 
-    cheat_suite_init(&suite, argv[0]);
+	cheat_suite_init(&suite, argv[0]);
 
-    suite.nofork = 0;
+	suite.nofork = 0;
 
-    if (argc > 1) {
-        if (argv[1][0] == '-') {
-            if (strcmp(argv[1], "--nofork") == 0) {
-                suite.nofork = 1;
-            }
-        } else {
-            for (i = 0; i < cheat_test_count; ++i) {
-                struct cheat_test_s const current_test = cheat_tests[i];
+	if (argc > 1) {
+		if (argv[1][0] == '-') {
+			if (strcmp(argv[1], "--nofork") == 0) {
+				suite.nofork = 1;
+			}
+		} else {
+			for (i = 0; i < cheat_test_count; ++i) {
+				struct cheat_test_s const current_test = cheat_tests[i];
 
-                if (strcmp(argv[1], current_test.name) == 0) {
-                    return run_test(&current_test, &suite);
-                }
-            }
+				if (strcmp(argv[1], current_test.name) == 0) {
+					return run_test(&current_test, &suite);
+				}
+			}
 
-            return -1;
-        }
-    }
+			return -1;
+		}
+	}
 
-    for (i = 0; i < cheat_test_count; ++i) {
-        struct cheat_test_s const current_test = cheat_tests[i];
+	for (i = 0; i < cheat_test_count; ++i) {
+		struct cheat_test_s const current_test = cheat_tests[i];
 
-        if (suite.nofork) {
-            run_test(&current_test, &suite);
-        } else {
-            run_isolated_test(&current_test, &suite);
-        }
+		if (suite.nofork) {
+			run_test(&current_test, &suite);
+		} else {
+			run_isolated_test(&current_test, &suite);
+		}
 
-        cheat_test_end(&suite);
-    }
+		cheat_test_end(&suite);
+	}
 
-    cheat_suite_summary(&suite);
+	cheat_suite_summary(&suite);
 
-    return (int )suite.test_failures; /* TODO truncate */
+	return (int )suite.test_failures; /* TODO truncate */
 }
 
 /* Third pass: Function definitions */
--- cheat_helpers.h
+++ cheat_helpers.h
@@ -8,46 +8,46 @@
 int cheat_stream_contains(FILE *stream, char const *contents);
 
 #define CHEAT_WRAP_STREAM(stream, stream_fd, name, body) \
-        char filename_pattern[] = "cheat_captured_stream_" #name "_XXXXXX";\
-        int original_stream;\
-        int fake_stream;\
-        fake_stream = mkstemp(filename_pattern);\
-        fflush(stream);\
-        original_stream = dup(stream_fd); \
-        dup2(fake_stream, stream_fd);\
-        setbuf(stream, NULL);\
-        body\
-        fflush(stream);\
-        dup2(original_stream, stream_fd);
+		char filename_pattern[] = "cheat_captured_stream_" #name "_XXXXXX";\
+		int original_stream;\
+		int fake_stream;\
+		fake_stream = mkstemp(filename_pattern);\
+		fflush(stream);\
+		original_stream = dup(stream_fd); \
+		dup2(fake_stream, stream_fd);\
+		setbuf(stream, NULL);\
+		body\
+		fflush(stream);\
+		dup2(original_stream, stream_fd);
 
 #define TEST_WITH_CAPTURED_STDOUT(name, body) TEST(name, { CHEAT_WRAP_STREAM(stdout, STDOUT_FILENO, name, body) } )
 #define TEST_WITH_CAPTURED_STDERR(name, body) TEST(name, { CHEAT_WRAP_STREAM(stderr, STDERR_FILENO, name, body) } )
 #define TEST_WITH_CAPTURED_OUTPUT(name, body) TEST(name, {\
-        CHEAT_WRAP_STREAM(stderr, STDERR_FILENO, name,\
-            CHEAT_WRAP_STREAM(stdout, STDOUT_FILENO, name, body)\
-        ))
+		CHEAT_WRAP_STREAM(stderr, STDERR_FILENO, name,\
+			CHEAT_WRAP_STREAM(stdout, STDOUT_FILENO, name, body)\
+		))
 
 int cheat_stream_contains(FILE *stream, char const *contents)
 {
-    char *buffer;
-    int result;
-    size_t len;
+	char *buffer;
+	int result;
+	size_t len;
 
-    const long int what = ftell(stream);
-    if (what == -1)
-        exit(42);
-    len = (size_t )what;
-    fseek(stream, 0, SEEK_SET);
+	const long int what = ftell(stream);
+	if (what == -1)
+		exit(42);
+	len = (size_t )what;
+	fseek(stream, 0, SEEK_SET);
 
-    buffer = malloc(len + 1);
-    fread(buffer, 1, len, stream);
-    buffer[len] = '\0';
+	buffer = malloc(len + 1);
+	fread(buffer, 1, len, stream);
+	buffer[len] = '\0';
 
-    result = strstr(buffer, contents) != NULL;
+	result = strstr(buffer, contents) != NULL;
 
-    free(buffer);
+	free(buffer);
 
-    return result;
+	return result;
 }
 
 #if _POSIX_C_SOURCE >= 200112L
@@ -63,28 +63,28 @@ int cheat_stream_contains(FILE *stream, char const *contents)
 #define dup2 _dup2
 
 int mkstemp(char * pattern) {
-    /*
-     * TODO: Generate a uUnique to avoid using CREATE_ALWAYS.
-     * Even better: get rid of this, and try to open in a do..while loop
-     * to match mkstemp.
-     */
-    char tempFileName[MAX_PATH];
-    GetTempFileName(
-        ".",
-        pattern,
-        0,
-        tempFileName);
+	/*
+	 * TODO: Generate a uUnique to avoid using CREATE_ALWAYS.
+	 * Even better: get rid of this, and try to open in a do..while loop
+	 * to match mkstemp.
+	 */
+	char tempFileName[MAX_PATH];
+	GetTempFileName(
+		".",
+		pattern,
+		0,
+		tempFileName);
 
-    HANDLE tempFile = CreateFile(
-        tempFileName,
-        GENERIC_READ | GENERIC_WRITE,
-        0,
-        NULL,
-        CREATE_ALWAYS, /* Bad. Should be _NEW, to avoid races. */
-        0,
-        NULL);
+	HANDLE tempFile = CreateFile(
+		tempFileName,
+		GENERIC_READ | GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS, /* Bad. Should be _NEW, to avoid races. */
+		0,
+		NULL);
 
-    return _open_osfhandle((intptr_t)tempFile, _O_APPEND);
+	return _open_osfhandle((intptr_t)tempFile, _O_APPEND);
 }
 
 #else
