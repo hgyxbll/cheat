6ec610eb59d3b1909203f8eda9bc251291af1f7d
Sampsa Kiiskinen <tuplanolla@gmail.com>
2014-07-01 00:32:19 +0300

    Quotes are ridiculous.

--- makefile.bat
+++ makefile.bat
@@ -1,10 +1,10 @@
 set cc=cl
 set warnings=/W4 ^
-	/wd4127
+	/wd4028 /wd4127 /wd4701
 set debug=%warnings% /Od /Zi
 set deploy=/Ox
 set cflags=%debug% ^
 	/D__STDC_VERSION__=199409L ^
 	/I.
 
-%cc% %cflags% /D__BASE_FILE__=\"cheat-example.c\" /Fecheat-example cheat-example.c
+%cc% %cflags% /D__BASE_FILE__="""cheat-example.c""" /Fecheat-example cheat-example.c
