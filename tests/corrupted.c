5dd2efecaff30d604628692f9bd6dd845b4f042e
Sampsa Kiiskinen <tuplanolla@gmail.com>
2014-07-22 02:02:52 +0300

    More ways to crash.

--- tests/corrupted.c
+++ tests/corrupted.c
@@ -4,15 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-CHEAT_TEST(overflow_crash,
+CHEAT_TEST(assignment_crash,
+	*(unsigned char* )NULL = UCHAR_MAX;
+)
+
+CHEAT_TEST(smash_crash,
+	unsigned char buffer[1];
+
+	memset(&buffer[-sizeof (int)], UCHAR_MAX, sizeof (int) * 3);
+)
+
+CHEAT_TEST(free_crash,
 	unsigned char* buffer;
 
 	buffer = malloc(BUFSIZ);
-	if (buffer != NULL)
-		memset(&buffer[-BUFSIZ], UCHAR_MAX, BUFSIZ * 3);
 	free(buffer);
-)
-
-CHEAT_TEST(assignment_crash,
-	*(unsigned char* )NULL = UCHAR_MAX;
+	free(buffer);
 )
